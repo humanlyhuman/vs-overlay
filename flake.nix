c255622007e1251e4c742fcca8d5c69b3b817b38
emily <ek@kyouma.net>
2025-04-19 14:54:46 +0200

    flake.nix: Fix outputs

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   outputs = { self, nixpkgs }:
     let
       inherit (nixpkgs) lib;
-      systems = [ "x86_64-linux" "aarch64-linux" "i686-linux" "x86_64-darwin" ];
+      systems = [ "x86_64-linux" "aarch64-linux" "i686-linux" ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
       nixpkgsFor = forAllSystems (system:
         import nixpkgs {
@@ -21,13 +21,13 @@
       overlay = import ./default.nix;
       packages = forAllSystems (system: {
         inherit (nixpkgsFor.${system})
-          getnative
-          vapoursynthPlugins;
-      });
+          getnative;
+      } // lib.filterAttrs (_: lib.isDerivation) nixpkgsFor.${system}.vapoursynthPlugins);
+
       hydraJobs = forAllSystems (system:
         lib.filterAttrs (_:
           lib.isDerivation
-        ) self.packages.${system}.vapoursynthPlugins
+        ) nixpkgsFor.${system}.vapoursynthPlugins
       );
     };
 }
