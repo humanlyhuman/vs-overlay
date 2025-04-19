3573e689693c2b235b47b5b59af752933f024cf4
emily <ek@kyouma.net>
2025-04-19 23:19:39 +0200

    lsmashsource: 1194.0.0.0 -> 1194.0.0.0-unstable-2025-04-13
    Diff: https://github.com/HomeOfAviSynthPlusEvolution/L-SMASH-Works/compare/1194.0.0.0...b43f75fab4a6f7330e24855c9f7c9c971cd626d9

--- plugins/lsmashsource/default.nix
+++ plugins/lsmashsource/default.nix
@@ -1,14 +1,27 @@
-{ lib, stdenv, fetchFromGitHub, pkg-config, which, vapoursynth, ffmpeg, l-smash }:
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  pkg-config,
+  meson,
+  ninja,
+  nasm,
+  which,
+  vapoursynth,
+  ffmpeg,
+  l-smash,
+  xxHash,
+}:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "lsmashsource";
-  version = "1194.0.0.0";
+  version = "b43f75fab4a6f7330e24855c9f7c9c971cd626d9";
 
   src = fetchFromGitHub {
     owner = "HomeOfAviSynthPlusEvolution";
     repo = "L-SMASH-Works";
     rev = finalAttrs.version;
-    sha256 = "1pb8rrh184pxy5calwfnmm02i0by8vc91c07w4ygj50y8yfqa3br";
+    hash = "sha256-64e1tDNFVyJnH0q41K9PT5YOxginhTYaj9NtdPu/g6A=";
   };
 
   preConfigure = ''
@@ -16,13 +29,39 @@ stdenv.mkDerivation (finalAttrs: {
     cd VapourSynth
   '';
 
-  nativeBuildInputs = [ pkg-config which ];
-  buildInputs = [ vapoursynth ffmpeg l-smash ];
+  postPatch = ''
+    substituteInPlace VapourSynth/meson.build \
+        --replace "vapoursynth_dep.get_pkgconfig_variable('libdir')" "get_option('libdir')"
+  '';
+
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+    which
+  ];
+  buildInputs = [
+    l-smash
+    vapoursynth
+    xxHash
+    (ffmpeg.override {
+      source = fetchFromGitHub {
+        owner = "HomeOfAviSynthPlusEvolution";
+        repo = "FFmpeg";
+        rev = "custom-patches-for-lsmashsource";
+        hash = "sha256-eMnn8G63BUU/dUWW8JIMkr67DI7atxmNpkYGwnnL37E=";
+      };
+      yasm = nasm;
+    })
+  ];
 
   meta = with lib; {
     description = "L-SMASH source plugin for VapourSynth";
     homepage = "https://github.com/VFR-maniac/L-SMASH-Works";
-    license = with licenses; [ isc lgpl21Plus ];
+    license = with licenses; [
+      isc
+      lgpl21Plus
+    ];
     maintainers = with maintainers; [ ];
     platforms = platforms.all;
   };
