12a8671bd2495c307a3b40e1d1771219c0872d05
emily <ek@kyouma.net>
2025-04-19 22:18:08 +0200

    vsgan: 1.6.4-unstable-2025-02-03 -> 1.6.4-unstable-2025-02-03
    Diff: https://github.com/rlaphoenix/VSGAN/compare/6f9d76babc6f97b6a17830733c379c5f8c522c31...6f9d76babc6f97b6a17830733c379c5f8c522c31

--- plugins/vsgan/default.nix
+++ plugins/vsgan/default.nix
@@ -9,12 +9,12 @@
 
 buildPythonPackage rec {
   pname = "vsgan";
-  version = "1.6.4";
+  version = "1.6.4-unstable-2025-02-03";
 
   src = fetchFromGitHub {
     owner = "rlaphoenix";
     repo = "VSGAN";
-    rev = "v${version}";
+    rev = "6f9d76babc6f97b6a17830733c379c5f8c522c31";
     sha256 = "sha256-bMgCah3kkyxNU5tb/eLt0tuG4xnD4sbtAzUK0a4uOKE=";
   };
 
