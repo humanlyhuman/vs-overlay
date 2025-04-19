41778565f7b2676573a59721e83fbc5a3c220d72
emily <ek@kyouma.net>
2025-04-19 13:08:39 +0200

    Add hydraJobs

--- flake.nix
+++ flake.nix
@@ -3,13 +3,17 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   outputs = { self, nixpkgs }:
     let
+      inherit (nixpkgs) lib;
       systems = [ "x86_64-linux" "aarch64-linux" "i686-linux" "x86_64-darwin" ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
       nixpkgsFor = forAllSystems (system:
         import nixpkgs {
           inherit system;
           overlays = [ self.overlay ];
-          config.allowUnfree = true;
+          config = {
+            allowUnfree = true;
+            allowUnsupportedSystem = true;
+          };
         }
       );
     in
@@ -20,5 +24,10 @@
           getnative
           vapoursynthPlugins;
       });
+      hydraJobs = forAllSystems (system:
+        lib.filterAttrs (_:
+          lib.isDerivation
+        ) self.packages.${system}.vapoursynthPlugins
+      );
     };
 }
