548040d4db60a4eb61f1486b2da5869ac53bc323
emily <ek@kyouma.net>
2025-04-20 15:09:40 +0200

    flake: add formatter

--- flake.nix
+++ flake.nix
@@ -1,12 +1,13 @@
 {
   description = "vs-overlay";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
   outputs = { self, nixpkgs }@inputs:
     let
       inherit (nixpkgs) lib;
       buildPlatforms = import ./lib/platforms.nix inputs;
       eachSystem = uwu: lib.mapAttrs uwu buildPlatforms;
-      nixpkgsFor = eachSystem (system: _:
+      pkgs = eachSystem (system: _:
         import nixpkgs {
           inherit system;
           overlays = [ self.overlays.default ];
@@ -20,10 +21,12 @@
     in
     {
       overlays.default = import ./default.nix;
+      formatter = eachSystem (system: _: pkgs.${system}.nixfmt-rfc-style);
+
       packages = eachSystem (system: platform:
         lib.filterAttrs (_: pkg: lib.meta.availableOn platform pkg) ({
-          inherit (nixpkgsFor.${system}) getnative;
-        } // lib.filterAttrs (_: lib.isDerivation) nixpkgsFor.${system}.vapoursynthPlugins)
+          inherit (pkgs.${system}) getnative;
+        } // lib.filterAttrs (_: lib.isDerivation) pkgs.${system}.vapoursynthPlugins)
       );
 
       hydraJobs = { inherit (self) packages; };
