ac8c013a7eb4901deb20147d1b5240e7d080dd1c
humanlyhuman <humanlyhuman420@pm.me>
2026-04-26 14:43:46 +0200

    Add postInstall step for libaddgrain.so placement
    Added postInstall step to move libaddgrain.so to the correct directory.

--- plugins/addgrain/default.nix
+++ plugins/addgrain/default.nix
@@ -7,7 +7,6 @@
   pkg-config,
   vapoursynth,
 }:
-
 stdenv.mkDerivation rec {
   pname = "vapoursynth-addgrain";
   version = "10";
@@ -17,23 +16,21 @@ stdenv.mkDerivation rec {
     rev = "r${version}";
     sha256 = "sha256-HNdYDpoyhWkpZZhcji2tWxWTojXKTKBbvm+iHp6Zdeo=";
   };
-
   nativeBuildInputs = [
     meson
     ninja
     pkg-config
   ];
-
   buildInputs = [ vapoursynth ];
   postPatch = ''
     substituteInPlace meson.build \
         --replace "vapoursynth_dep.get_pkgconfig_variable('libdir')" "get_option('libdir')"
   '';
-
-  mesonFlags = [
-    "--libdir=${placeholder "out"}/lib/vapoursynth"
-  ];
-
+  postInstall = ''
+    mkdir -p $out/lib/vapoursynth
+    find $out -name "libaddgrain.so" ! -path "*/vapoursynth/*" \
+      -exec mv {} $out/lib/vapoursynth/ \;
+  '';
   meta = with lib; {
     description = "AddGrain filter for VapourSynth";
     homepage = "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-AddGrain";
