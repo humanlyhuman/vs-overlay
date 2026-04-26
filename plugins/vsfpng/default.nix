d981bb3df2e2b891f034057a242ed336823af5e1
humanlyhuman <humanlyhuman420@pm.me>
2026-04-26 17:45:35 +0200

    Create default.nix for vsfpng plugin
    Add Nix expression for vsfpng VapourSynth plugin

---
+++ plugins/vsfpng/default.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  meson,
+  ninja,
+  pkg-config,
+  vapoursynth,
+  python3,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "vsfpng";
+  version = "1.2";
+
+  src = fetchFromGitHub {
+    owner = "Mikewando";
+    repo = "vsfpng";
+    rev = "v${version}";
+    sha256 = "sha256-+OYUAp6T+ZGSFixw7W/QsqXVlPYea83WV88EVsI11KM=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+  ];
+
+  buildInputs = [
+    vapoursynth
+  ];
+
+  # install into VapourSynth plugin dir
+  mesonFlags = [
+    "-Dlibdir=${placeholder "out"}/lib/vapoursynth"
+  ];
+
+  meta = with lib; {
+    description = "fpng plugin for VapourSynth";
+    homepage = "https://github.com/Mikewando/vsfpng";
+    license = licenses.lgpl21;
+    platforms = platforms.all;
+  };
+}
