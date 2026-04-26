d8fd2617b1d6aa81d22ae92d8da8c482c8105616
humanlyhuman <humanlyhuman420@pm.me>
2026-04-26 11:39:08 +0200

    Update default.nix

--- plugins/deblock/default.nix
+++ plugins/deblock/default.nix
@@ -5,34 +5,31 @@
   meson,
   ninja,
   pkg-config,
+  python3,
   vapoursynth,
 }:
-
 stdenv.mkDerivation rec {
   pname = "VapourSynth-Deblock";
   version = "8";
-
   src = fetchFromGitHub {
     owner = "HomeOfVapourSynthEvolution";
     repo = pname;
     rev = "r${version}";
     sha256 = "sha256-ipQfEZY5ZD+notSD1jJxyxoU3zpGBRhcOIuz808TDn4=";
   };
-
   nativeBuildInputs = [
     meson
     ninja
     pkg-config
+    python3
   ];
   buildInputs = [ vapoursynth ];
-
   postPatch = ''
     substituteInPlace meson.build \
-        --replace "vapoursynth_dep.get_pkgconfig_variable('libdir')" "get_option('libdir')"
+        --replace-warn "vapoursynth_dep.get_pkgconfig_variable('libdir')" "get_option('libdir')"
   '';
-
   meta = with lib; {
-    description = "A Deblock filter plugin for VapourSynth.";
+    description = "A Deblock filter plugin for VapourSynth";
     homepage = "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-Deblock";
     license = licenses.gpl2Plus;
     maintainers = with maintainers; [ sbruder ];
