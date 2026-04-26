501962d985eb5a9732905d44ee31125f30141a27
humanlyhuman <humanlyhuman420@pm.me>
2026-04-26 16:26:39 +0200

    Refactor vapoursynth-scxvid derivation structure

--- plugins/scxvid/default.nix
+++ plugins/scxvid/default.nix
@@ -10,49 +10,41 @@
   python3
 }:
 
-stdenv.mkDerivation rec {
-  pname = "vapoursynth-scxvid";
-  version = "3";
-
-  src = fetchFromGitHub {
-    owner = "dubhater";
-    repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-WgoIF7ni2j6wNCutysV18B693OapzniZoy94iyZR3uA=";
-  };
-
-  nativeBuildInputs = [
-    meson
-    ninja
-    pkg-config
-    python3
-  ];
-
-  buildInputs = [
-    vapoursynth
-    xvidcore
-  ];
-
-  postPatch = ''
-    substituteInPlace meson.build \
-      --replace-fail \
-        "run_command(py, '-c', 'import vapoursynth as vs; print(vs.get_include())', check: true).stdout().strip()" \
-        "dependency('vapoursynth').get_variable(pkgconfig: 'includedir')" \
-      --replace-fail \
-        "py.get_install_dir() / 'vapoursynth/plugins'" \
-        "get_option('libdir') / 'vapoursynth'"
-    substituteInPlace requirements.txt \
-        --replace "VapourSynth>=69" ""
-  '';
-
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p $out/lib/vapoursynth
-    cp build/src/libscxvid.so $out/lib/vapoursynth/
-
-    runHook postInstall
-  '';
+  stdenv.mkDerivation rec {
+    pname = "vapoursynth-scxvid";
+    version = "3";
+  
+    src = fetchFromGitHub {
+      owner = "dubhater";
+      repo = pname;
+      rev = "v${version}";
+      sha256 = "sha256-WgoIF7ni2j6wNCutysV18B693OapzniZoy94iyZR3uA=";
+    };
+  
+    nativeBuildInputs = [ meson ninja pkg-config python3 ];
+  
+    buildInputs = [ vapoursynth xvidcore ];
+  
+    postPatch = ''
+      substituteInPlace meson.build \
+        --replace-fail \
+          "deps = [xvid_dep]" \
+          "vapoursynth_dep = dependency('vapoursynth')\ndeps = [xvid_dep, vapoursynth_dep]" \
+        --replace-fail \
+          "incdir = include_directories(" \
+          "# disabled python include detection\n# incdir = include_directories(" \
+        --replace-fail \
+          "include_directories: incdir," \
+          "" \
+        --replace-fail \
+          "py.get_install_dir() / 'vapoursynth/plugins'" \
+          "get_option('libdir') / 'vapoursynth'"
+    '';
+  
+    installPhase = ''
+      mkdir -p $out/lib/vapoursynth
+      cp build/src/scxvid.so $out/lib/vapoursynth/
+    '';
 
   meta = with lib; {
     description = "Scene change detection plugin for VapourSynth using xvid";
