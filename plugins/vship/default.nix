845e6ded042a79dab8f1f2cbd1cf5271d2e30973
humanlyhuman <humanlyhuman420@pm.me>
2026-04-26 17:16:58 +0200

    Update default.nix

--- plugins/vship/default.nix
+++ plugins/vship/default.nix
@@ -1,7 +1,8 @@
 {
   lib,
   stdenv,
-  fetchFromGitHub,
+  fetchFromGitHub, # still usable for Codeberg via fetchgit if preferred
+  fetchgit,
   hostPlatform,
   rocmPackages,
   vapoursynth,
@@ -9,13 +10,12 @@
 
 stdenv.mkDerivation rec {
   pname = "vship";
-  version = "2.2";
+  version = "5.0.1";
 
-  src = fetchFromGitHub {
-    owner = "Line-fr";
-    repo = "Vship";
+  src = fetchgit {
+    url = "https://codeberg.org/Line-fr/Vship.git";
     rev = "v${version}";
-    hash = "sha256-uA2pxv46IVD6/ynoxlS6aB3agyq0AgaQU3I2GqfssNo=";
+    sha256 = "sha256-JkWw1dTBb47CBLG3fGXtqHTEwbiX9IDwKHroJtY9s+A=";
   };
 
   nativeBuildInputs = [
@@ -39,7 +39,7 @@ stdenv.mkDerivation rec {
   buildPhase = ''
     echo $PATH
     hipcc src/main.cpp \
-      -I "${vapoursynth}/include/vapoursynth"
+      -I "${vapoursynth}/include/vapoursynth" \
       --offload-arch=gfx1100,gfx1101,gfx1102,gfx1030,gfx1031,gfx1032,gfx906,gfx801,gfx802,gfx803 \
       -Wno-unused-result -Wno-ignored-attributes -shared -fPIC \
       -o "vship${hostPlatform.extensions.sharedLibrary}" -v
@@ -49,12 +49,12 @@ stdenv.mkDerivation rec {
     install -D -t "$out/lib/vapoursynth" vship${hostPlatform.extensions.sharedLibrary}
   '';
 
-  meta = {
-    description = "A high-performance VapourSynth plugin for GPU-accelerated visual fidelity metrics, focusing on SSIMULACRA2 & Butteraugli";
-    homepage = "https://github.com/Line-fr/Vship";
-    license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ snaki ];
+  meta = with lib; {
+    description = "GPU-accelerated VapourSynth plugin for SSIMULACRA2 & Butteraugli metrics";
+    homepage = "https://codeberg.org/Line-fr/Vship";
+    license = licenses.mit;
+    maintainers = with maintainers; [ snaki ];
     mainProgram = "vship";
-    platforms = lib.platforms.all;
+    platforms = platforms.all;
   };
 }
