3f4be03b2d0d0293e7298620aec663631df217a4
humanlyhuman <humanlyhuman420@pm.me>
2026-04-26 10:31:27 +0200

    Create Nix expression for vsjetpack package
    Add default.nix for building vsjetpack package

---
+++ plugins/vsjetpack/default.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  vapoursynth,
+  hatchling,
+  versioningit,
+  jetpytools,
+  numpy,
+}:
+buildPythonPackage rec {
+  pname = "vsjetpack";
+  version = "1.4.1";
+  
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "Jaded-Encoding-Thaumaturgy";
+    repo = "vs-jetpack";
+    rev = "v${version}";
+    sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  };
+
+  build-system = [
+    hatchling
+    versioningit
+  ];
+
+  dependencies = [
+    vapoursynth
+    jetpytools
+    numpy
+  ];
+
+  pythonImportsCheck = [ ];
+  doCheck = false;
+
+  meta = with lib; {
+    description = "Full suite of filters, wrappers, and helper functions for VapourSynth";
+    homepage = "https://github.com/Jaded-Encoding-Thaumaturgy/vs-jetpack";
+    license = licenses.mit;
+    maintainers = [ ];
+    platforms = platforms.all;
+  };
+}
