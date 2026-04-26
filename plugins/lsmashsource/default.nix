34ac2d073f52610b9a81757dbc413e56ed6681d2
humanlyhuman <humanlyhuman420@pm.me>
2026-04-26 13:21:28 +0200

    Refactor ffmpeg integration and clean up dependencies

--- plugins/lsmashsource/default.nix
+++ plugins/lsmashsource/default.nix
@@ -10,30 +10,37 @@
   vapoursynth,
   ffmpeg,
   l-smash,
-  xxHash
+  xxHash,
 }:
-
+let
+  ffmpeg-lsmash = (ffmpeg.override {
+    source = fetchFromGitHub {
+      owner = "HomeOfAviSynthPlusEvolution";
+      repo = "FFmpeg";
+      rev = "custom-patches-for-lsmashsource";
+      hash = "sha256-vbnrdHOzANc+EXKr4SAW9Hcorbgih/apsFoTaJlIITQ=";
+    };
+  }).overrideAttrs (_old: {
+    patches = [];
+  });
+in
 stdenv.mkDerivation (finalAttrs: {
   pname = "lsmashsource";
   version = "1282";
-
   src = fetchFromGitHub {
     owner = "HomeOfAviSynthPlusEvolution";
     repo = "L-SMASH-Works";
     rev = finalAttrs.version;
     hash = "sha256-0TSK5nvLAOwn6BrEFuMd342i+rAyu6JLWWk4czx9RXI=";
   };
-
   preConfigure = ''
     patchShebangs .
     cd VapourSynth
   '';
-
   postPatch = ''
     substituteInPlace VapourSynth/meson.build \
         --replace "vapoursynth_dep.get_pkgconfig_variable('libdir')" "get_option('libdir')"
   '';
-
   nativeBuildInputs = [
     meson
     ninja
@@ -43,19 +50,9 @@ stdenv.mkDerivation (finalAttrs: {
   buildInputs = [
     l-smash
     vapoursynth
-    xxhash
-    (ffmpeg.override {
-      source = fetchFromGitHub {
-        owner = "HomeOfAviSynthPlusEvolution";
-        repo = "FFmpeg";
-        rev = "custom-patches-for-lsmashsource";
-        hash = "sha256-vbnrdHOzANc+EXKr4SAW9Hcorbgih/apsFoTaJlIITQ=";
-      };
-    }).overrideAttrs (old: {
-      patches = [];
-    })
+    xxHash
+    ffmpeg-lsmash
   ];
-
   meta = with lib; {
     description = "L-SMASH source plugin for VapourSynth";
     homepage = "https://github.com/VFR-maniac/L-SMASH-Works";
