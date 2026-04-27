a94c5e8a36d546c829253bdd8ae87c2c49bfb7f7
humanlyhuman <humanlyhuman420@pm.me>
2026-04-27 09:53:14 +0200

    Create Nix expression for vapoursynth-edgemasks
    Add default.nix for VapourSynth EdgeMasks package

---
+++ plugins/edgemasks/default.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  meson,
+  ninja,
+  pkg-config,
+  vapoursynth,
+}:
+stdenv.mkDerivation rec {
+  pname = "vapoursynth-edgemasks";
+  version = "4";
+
+  src = fetchFromGitHub {
+    owner = "HolyWu";
+    repo = "VapourSynth-EdgeMasks";
+    rev = "r${version}";
+    sha256 = "sha256-D7oqGhDrT/vMVq2a0XzQHx6jDnvB1pFpMOG/0rDkyjM=";
+  };
+
+  postPatch = ''
+    substituteInPlace meson.build \
+      --replace \
+        "run_command(py, '-c', 'import vapoursynth as vs; print(vs.get_include())', check: true).stdout().strip()" \
+        "'${vapoursynth}/include/vapoursynth'" \
+      --replace \
+        "py.get_install_dir() / 'vapoursynth/plugins'" \
+        "'${placeholder "out"}/lib/vapoursynth'"
+  '';
+
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+  ];
+  buildInputs = [ vapoursynth ];
+
+  meta = with lib; {
+    description = "EdgeMasks filter for VapourSynth";
+    homepage = "https://github.com/HolyWu/VapourSynth-EdgeMasks";
+    license = licenses.mit;
+    platforms = platforms.all;
+  };
+}
