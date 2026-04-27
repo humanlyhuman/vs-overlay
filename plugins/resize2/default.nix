8355168ab9e3cb3db957bbbeea2a05c229ad1370
humanlyhuman <humanlyhuman420@pm.me>
2026-04-27 20:27:28 +0200

    Update default.nix

--- plugins/resize2/default.nix
+++ plugins/resize2/default.nix
@@ -48,24 +48,25 @@ buildPythonPackage rec {
   ];
 
   dontCheckRuntimeDeps = true;
-  postPatch = ''
-      rm -rf subprojects/zimg
-      cp -r ${zimgSrc} subprojects/zimg
-      chmod -R +w subprojects/zimg
+postPatch = ''
+  mkdir -p subprojects/packagecache
+  rm -rf subprojects/packagecache/zimg
+  cp -r ${zimgSrc} subprojects/packagecache/zimg
+  chmod -R +w subprojects/packagecache/zimg
 
-      python <<EOF
-    import re
-    p = open("pyproject.toml").read()
-    p = re.sub(r'"vapoursynth>=.*?",?', "", p)
-    p = re.sub(r'"ninja==.*?",?', '"ninja",', p)
-    open("pyproject.toml", "w").write(p)
-    EOF
+  python <<EOF
+import re
+p = open("pyproject.toml").read()
+p = re.sub(r'"vapoursynth>=.*?",?', "", p)
+p = re.sub(r'"ninja==.*?",?', '"ninja",', p)
+open("pyproject.toml", "w").write(p)
+EOF
 
-      substituteInPlace meson.build \
-        --replace-fail \
-        "import vapoursynth as vs; print(vs.get_include())" \
-        "print(\"${vapoursynth}/include/vapoursynth\")"
-  '';
+  substituteInPlace meson.build \
+    --replace-fail \
+    "import vapoursynth as vs; print(vs.get_include())" \
+    "print(\"${vapoursynth}/include/vapoursynth\")"
+'';
 
   postInstall = ''
     mkdir -p $out/lib/vapoursynth
