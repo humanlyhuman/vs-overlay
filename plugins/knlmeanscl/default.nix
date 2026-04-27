6c6909ffda93281f476fdcb59a40d872cd0d1cdf
humanlyhuman <humanlyhuman420@pm.me>
2026-04-27 18:26:21 +0200

    Refactor default.nix for vapoursynth-knlmeanscl

--- plugins/knlmeanscl/default.nix
+++ plugins/knlmeanscl/default.nix
@@ -1,17 +1,22 @@
 {
   lib,
-  stdenv,
+  buildPythonPackage,
   fetchFromGitHub,
-  which,
+  hatchling,
+  meson,
+  ninja,
+  packaging,
   pkg-config,
   vapoursynth,
   boost,
   opencl-headers,
   ocl-icd,
 }:
-stdenv.mkDerivation rec {
-  pname = "knlmeanscl";
+
+buildPythonPackage rec {
+  pname = "vapoursynth-knlmeanscl";
   version = "1.2";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Jaded-Encoding-Thaumaturgy";
@@ -20,10 +25,17 @@ stdenv.mkDerivation rec {
     hash = "sha256-mFOcKqUzDhWu7yiqiHReTFSzb5jA/YDPb7IOASX9JUo=";
   };
 
+  build-system = [
+    hatchling
+    meson
+    ninja
+    packaging
+  ];
+
   nativeBuildInputs = [
-    which
     pkg-config
   ];
+
   buildInputs = [
     vapoursynth
     boost
@@ -31,18 +43,26 @@ stdenv.mkDerivation rec {
     ocl-icd
   ];
 
-  configureFlags = [
-    "--install=${placeholder "out"}/lib/vapoursynth"
-  ];
+  dependencies = [ vapoursynth ];
 
-  preInstall = ''
-    mkdir -p $out/lib/vapoursynth
+  postPatch = ''
+    sed -i '/vapoursynth>=74/d' pyproject.toml
   '';
 
+  postInstall = ''
+    mkdir -p $out/lib/vapoursynth
+
+    ln -s \
+      $out/lib/python*/site-packages/vapoursynth/plugins/libknlmeanscl.so \
+      $out/lib/vapoursynth/libknlmeanscl.so
+  '';
+
+  doCheck = false;
+
   meta = with lib; {
-    description = "An optimized OpenCL implementation of the Non-local means de-noising algorithm";
+    description = "Optimized OpenCL implementation of the Non-local means de-noising algorithm";
     homepage = "https://github.com/Jaded-Encoding-Thaumaturgy/vapoursynth-knlmeanscl";
-    license = licenses.gpl3;
-    platforms = platforms.all;
+    license = licenses.gpl3Plus;
+    platforms = platforms.linux;
   };
 }
