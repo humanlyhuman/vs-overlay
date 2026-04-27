1d2a263e32f445a719699b379329fab2f5d530f6
humanlyhuman <humanlyhuman420@pm.me>
2026-04-27 21:17:29 +0200

    Update default.nix

--- tools/nativeres/default.nix
+++ tools/nativeres/default.nix
@@ -68,10 +68,11 @@ in
         --replace-fail '"vsjetengine>=1.2.0",' "" \
         --replace-fail '"vsjetpack>=1.3.0",' ""
     '';
-    postFixup = ''
-      wrapProgram $out/bin/nativeres \
-        --set VAPOURSYNTH_PLUGIN_PATH ${vapoursynth-with-plugins}/lib/vapoursynth
-    '';
+  postFixup = ''
+    wrapProgram $out/bin/nativeres \
+      --prefix PYTHONPATH : ${vapoursynth-with-plugins}/${python.sitePackages} \
+      --prefix PATH : ${vapoursynth-with-plugins}/bin
+  '';
     meta = with lib; {
       description = "Descale analysis tools for VapourSynth";
       homepage = "https://github.com/Jaded-Encoding-Thaumaturgy/nativeres";
