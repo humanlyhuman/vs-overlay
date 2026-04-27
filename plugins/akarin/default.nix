339cff75411c3ad7d42643525e921670355b7d96
humanlyhuman <humanlyhuman420@pm.me>
2026-04-27 10:42:17 +0200

    Refactor postPatch to improve readability

--- plugins/akarin/default.nix
+++ plugins/akarin/default.nix
@@ -29,22 +29,24 @@ buildPythonPackage {
   };
 
   postPatch = ''
-    substituteInPlace pyproject.toml \
-      --replace-fail "meson==1.11.0" "meson" \
-      --replace-fail "ninja==1.13.0" "ninja" \
-      --replace-fail "vapoursynth>=74" "vapoursynth"
-    substituteInPlace meson.build \
-      --replace-fail \
-        "py = import('python').find_installation(pure: false)
-r = run_command(
-  py,
-  '-c',
-  'import vapoursynth as vs; print(vs.get_include())',
-  check: true,
-)
-inc_vs = include_directories(r.stdout().strip())
-incdir += inc_vs" \
-        "deps += dependency('vapoursynth')"
+        substituteInPlace pyproject.toml \
+          --replace-fail "meson==1.11.0" "meson" \
+          --replace-fail "ninja==1.13.0" "ninja" \
+          --replace-fail "vapoursynth>=74" "vapoursynth"
+
+        substituteInPlace meson.build \
+          --replace-fail \
+            "py = import('python').find_installation(pure: false)
+
+    r = run_command(
+      py,
+      '-c',
+      'import vapoursynth as vs; print(vs.get_include())',
+      check: true,
+    )
+    inc_vs = include_directories(r.stdout().strip())
+    incdir += inc_vs" \
+            "deps += dependency('vapoursynth')"
   '';
 
   nativeBuildInputs = [
