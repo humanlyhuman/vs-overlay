20c3be1513bea76b633c4d3a635b110b8426d717
humanlyhuman <humanlyhuman420@pm.me>
2026-04-27 07:13:06 +0200

    Create default.nix for knlmeanscl package
    Add default.nix for building knlmeanscl package

---
+++ plugins/knlmeanscl/default.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  which,
+  pkg-config,
+  vapoursynth,
+  boost,
+  opencl-headers,
+  ocl-icd,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "knlmeanscl";
+  version = "1.2";
+
+  src = fetchFromGitHub {
+    owner = "Jaded-Encoding-Thaumaturgy";
+    repo = "vapoursynth-knlmeanscl";
+    rev = "v${version}";
+    sha256 = "sha256-mFOcKqUzDhWu7yiqiHReTFSzb5jA/YDPb7IOASX9JUo=";
+  };
+
+  dontAddPrefix = true;
+  configureFlags = [ "--install=$(out)/lib/vapoursynth" ];
+
+  nativeBuildInputs = [
+    which
+    pkg-config
+  ];
+  buildInputs = [
+    vapoursynth
+    boost
+    opencl-headers
+    ocl-icd
+  ];
+
+  meta = with lib; {
+    description = "An optimized OpenCL implementation of the Non-local means de-noising algorithm";
+    homepage = "https://github.com/Jaded-Encoding-Thaumaturgy/vapoursynth-knlmeanscl";
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ ];
+    platforms = platforms.all;
+  };
+}
