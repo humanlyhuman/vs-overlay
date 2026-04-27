8451ea187f73d3e323f439fbc49238fc372b72cb
humanlyhuman <humanlyhuman420@pm.me>
2026-04-27 20:40:08 +0200

    Create default.nix

---
+++ plugins/vs-jet-engine/default.nix
@@ -0,0 +1,71 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  vapoursynth,
+  hatchling,
+  versioningit,
+  trio ? null,
+  mypy,
+  pytest,
+  pytest-cov,
+  pytest-asyncio,
+  ruff,
+  vsstubs ? null,
+  python,
+}:
+
+buildPythonPackage rec {
+  pname = "vsjetengine";
+  version = "1.2.0";
+
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "Jaded-Encoding-Thaumaturgy";
+    repo = "vs-jet-engine";
+    rev = "v${version}";
+    hash = "sha256-dkFm8PwIitv84pDsAZZe71ZUF6faP4mxXTlyW5ioJ5A=";
+  };
+
+  build-system = [
+    hatchling
+    versioningit
+  ];
+
+  dependencies = [
+    vapoursynth
+  ];
+
+  optional-dependencies = {
+    trio = lib.optionals (trio != null) [ trio ];
+  };
+
+  nativeCheckInputs = [
+    mypy
+    pytest
+    pytest-cov
+    pytest-asyncio
+    ruff
+  ]
+  ++ lib.optionals (trio != null) [ trio ]
+  ++ lib.optionals (vsstubs != null && lib.versionAtLeast python.pythonVersion "3.13") [
+    vsstubs
+  ];
+
+  env.VERSIONINGIT_OVERRIDE_VCS = version;
+
+  pythonImportsCheck = [
+    "vsengine"
+  ];
+
+  doCheck = false;
+  dontCheckRuntimeDeps = true;
+
+  meta = with lib; {
+    description = "An engine for VapourSynth previewers, renderers and script analysis tools";
+    homepage = "https://github.com/Jaded-Encoding-Thaumaturgy/vs-jet-engine";
+    license = licenses.eupl12;
+    platforms = platforms.all;
+  };
+}
