a6ca17dbf53b720322540490f2c90b8c384acbe6
humanlyhuman <humanlyhuman420@pm.me>
2026-04-27 09:29:17 +0200

    Add awarp plugin to vapoursynthPlugins

--- default.nix
+++ default.nix
@@ -14,6 +14,7 @@ in
 {
   vapoursynthPlugins = prev.lib.recurseIntoAttrs {
     adaptivegrain = prev.callPackage ./plugins/adaptivegrain { };
+    awarp = prev.callPackage./plugins/awarp { };
     vs-noise = prev.callPackage ./plugins/vs-noise { };
     autocrop = prev.callPackage ./plugins/autocrop { };
     beziercurve = prev.callPackage ./plugins/beziercurve { };
