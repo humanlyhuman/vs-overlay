3261b81dbe93609600bb0b228e37b0486b87d76f
humanlyhuman <humanlyhuman420@pm.me>
2026-04-27 18:46:22 +0200

    Update default.nix

--- plugins/resize2/default.nix
+++ plugins/resize2/default.nix
@@ -1,69 +1,70 @@
 {
   lib,
-  stdenv,
   fetchFromGitHub,
   meson,
   ninja,
   pkg-config,
   vapoursynth,
   python,
-}: let
+  python3Packages,
+  stdenv,
+}:
+
+let
   pythonEnv = python.withPackages (ps: [
     ps.vapoursynth
   ]);
 in
-  python.pkgs.toPythonModule (
-    stdenv.mkDerivation (finalAttrs: {
-      pname = "vapoursynth-resize2";
-      version = "0.4.2";
 
-      src = fetchFromGitHub {
-        owner = "Jaded-Encoding-Thaumaturgy";
-        repo = finalAttrs.pname;
-        rev = "${finalAttrs.version}";
-        hash = "sha256-oOfDYHBZZ3JEYrbeiwSDNAaua7hlC61lYJOTqB6I7/Q=";
-      };
+python.pkgs.toPythonModule (
+  meson.buildMesonPackage rec {
+    pname = "vapoursynth-resize2";
+    version = "0.4.2";
 
-      nativeBuildInputs = [
-        meson
-        ninja
-        pkg-config
-        pythonEnv
-      ];
+    src = fetchFromGitHub {
+      owner = "Jaded-Encoding-Thaumaturgy";
+      repo = pname;
+      rev = version;
+      hash = "sha256-oOfDYHBZZ3JEYrbeiwSDNAaua7hlC61lYJOTqB6I7/Q=";
+    };
 
-      buildInputs = [
-        vapoursynth
-      ];
-      mesonFlags = ["--wrap-mode=forcefallback"];
-      postPatch = ''
-        substituteInPlace meson.build \
-          --replace-fail \
-          "py = import('python').find_installation()" \
-          "py = import('python').find_installation('${pythonEnv}/bin/python')"
+    nativeBuildInputs = [
+      pkg-config
+      pythonEnv
+    ];
 
-        substituteInPlace meson.build \
-          --replace-fail \
-          "vapoursynth_include_command = run_command(" \
-          "# patched out by nixpkgs\n# vapoursynth_include_command = run_command("
+    buildInputs = [
+      vapoursynth
+    ];
 
-        substituteInPlace meson.build \
-          --replace-fail \
-          "vapoursynth_include = include_directories(vapoursynth_include_command.stdout().strip())" \
-          "vapoursynth_include = include_directories('${vapoursynth}/include/vapoursynth')"
-      '';
+    mesonFlags = [
+      "--wrap-mode=forcefallback"
+    ];
 
-      postInstall = ''
-        mkdir -p $out/lib/vapoursynth
-        ln -s \
-          $out/${python.sitePackages}/vapoursynth/plugins/resize2${stdenv.hostPlatform.extensions.sharedLibrary} \
-          $out/lib/vapoursynth/resize2${stdenv.hostPlatform.extensions.sharedLibrary}
-      '';
+    postPatch = ''
+      substituteInPlace meson.build \
+        --replace-fail \
+        "py = import('python').find_installation()" \
+        "py = import('python').find_installation('${pythonEnv}/bin/python')"
 
-      meta = with lib; {
-        description = "resize2 plugin for VapourSynth with blur support";
-        homepage = "https://github.com/Jaded-Encoding-Thaumaturgy/vapoursynth-resize2";
-        license = licenses.lgpl21Only;
-        platforms = platforms.all;
-      };
-    })
-  )
+      substituteInPlace meson.build \
+        --replace-fail \
+        "vapoursynth_include = include_directories(vapoursynth_include_command.stdout().strip())" \
+        "vapoursynth_include = include_directories('${vapoursynth}/include/vapoursynth')"
+    '';
+
+    postInstall = ''
+      mkdir -p $out/lib/vapoursynth
+      ln -s \
+        $out/${python.sitePackages}/vapoursynth/plugins/resize2${stdenv.hostPlatform.extensions.sharedLibrary} \
+        $out/lib/vapoursynth/resize2${stdenv.hostPlatform.extensions.sharedLibrary}
+    '';
+
+    meta = with lib; {
+      description = "resize2 plugin for VapourSynth with blur support";
+      homepage = "https://github.com/Jaded-Encoding-Thaumaturgy/vapoursynth-resize2";
+      license = licenses.lgpl21Only;
+      platforms = platforms.all;
+    };
+  }
+)
