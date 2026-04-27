3e39053ff4a0afaef208412ca097a1a5e405d714
humanlyhuman <humanlyhuman420@pm.me>
2026-04-27 16:04:06 +0200

    Refactor plugin names for consistency

--- default.nix
+++ default.nix
@@ -18,12 +18,9 @@ in
     autocrop       = prev.callPackage ./plugins/autocrop { };
     awarp          = prev.callPackage ./plugins/awarp { };
     beziercurve    = prev.callPackage ./plugins/beziercurve { };
-    bifrost        = prev.callPackage ./plugins/bifrost { };
     bm3d           = prev.callPackage ./plugins/bm3d { };
     cas            = prev.callPackage ./plugins/cas { };
-    cnr2           = prev.callPackage ./plugins/cnr2 { };
     d2vsource      = prev.callPackage ./plugins/d2vsource { };
-    dctfilter      = prev.callPackage ./plugins/dctfilter { };
     deblock        = prev.callPackage ./plugins/deblock { };
     descale        = prev.callPackage ./plugins/descale { };
     dfttest        = prev.callPackage ./plugins/dfttest { };
@@ -33,28 +30,27 @@ in
     fillborders    = prev.callPackage ./plugins/fillborders { };
     fmtconv        = prev.callPackage ./plugins/fmtconv { };
     histogram      = prev.callPackage ./plugins/histogram { };
-    knlmeanscl     = prev.callPackage ./plugins/knlmeanscl { };
     lsmashsource   = prev.callPackage ./plugins/lsmashsource { };
     mvtools        = prev.vapoursynth-mvtools;
-    neo_f3kdb      = prev.callPackage ./plugins/neo_f3kdb { };
+    neo-f3kdb      = prev.callPackage ./plugins/neo_f3kdb { };
+    nlm-ispc       = prev.callPackage ./plugins/nlm-ispc { };
     ocr            = prev.callPackage ./plugins/ocr { };
     placebo        = prev.callPackage ./plugins/placebo { };
     readmpls       = prev.callPackage ./plugins/readmpls { };
     remap          = prev.callPackage ./plugins/remap { };
     removegrain    = prev.callPackage ./plugins/removegrain { };
-    sangnom        = prev.callPackage ./plugins/sangnom { };
-    scxvid         = prev.callPackage ./plugins/scxvid { };
+    sneedif        = prev.callPackage ./plugins/sneedif { };
     subtext        = prev.callPackage ./plugins/subtext { };
     tnlmeans       = prev.callPackage ./plugins/tnlmeans { };
     ttempsmooth    = prev.callPackage ./plugins/ttempsmooth { };
-    vsfpng         = prev.callPackage ./plugins/vsfpng { };
     vivtc          = prev.callPackage ./plugins/vivtc { };
+    vs-filtermod   = prev.callPackage ./plugins/vsfiltermod { };
+    vs-fpng        = prev.callPackage ./plugins/vsfpng { };
+    vs-hip         = prev.callPackage ./plugins/vship { };
     vs-mlrt        = prev.callPackage ./plugins/vs-mlrt { };
+    vs-ncnn        = prev.callPackage ./plugins/vs-mlrt/vsncnn { };
     vs-noise       = prev.callPackage ./plugins/vs-noise { };
-    vsfiltermod    = prev.callPackage ./plugins/vsfiltermod { };
-    vship          = prev.callPackage ./plugins/vship { };
-    vsncnn         = prev.callPackage ./plugins/vs-mlrt/vsncnn { };
-    vstrt          = prev.callPackage ./plugins/vs-mlrt/vstrt { };
+    vs-trt         = prev.callPackage ./plugins/vs-mlrt/vstrt { };
     znedi3         = prev.callPackage ./plugins/znedi3 { };
 
     # Python wrappers
@@ -67,15 +63,13 @@ in
     insaneaa       = callPythonPackage ./plugins/insaneaa { };
     jetpytools     = callPythonPackage ./plugins/jetpytools { };
     lvsfunc        = callPythonPackage ./plugins/lvsfunc { };
-    nnedi3_resample = callPythonPackage ./plugins/nnedi3_resample { };
-    nnedi3_rpow2   = callPythonPackage ./plugins/nnedi3_rpow2 { };
     rekt           = callPythonPackage ./plugins/rekt { };
     vardefunc      = callPythonPackage ./plugins/vardefunc { };
-    vsgan          = callPythonPackage ./plugins/vsgan { };
-    vsjetpack      = callPythonPackage ./plugins/vsjetpack {
+    vs-gan         = callPythonPackage ./plugins/vsgan { };
+    vs-jetpack     = callPythonPackage ./plugins/vsjetpack {
       inherit (final.vapoursynthPlugins) jetpytools;
     };
-    vsTAAmbk       = callPythonPackage ./plugins/vsTAAmbk { };
+    vs-taambk      = callPythonPackage ./plugins/vsTAAmbk { };
     vsutil         = callPythonPackage ./plugins/vsutil { };
   };
 
