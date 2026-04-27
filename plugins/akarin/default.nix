28e4246305992ad0d8dbd259aa014d58a622910f
humanlyhuman <humanlyhuman420@pm.me>
2026-04-27 10:38:10 +0200

    Create default.nix for vapoursynth-akarin plugin
    Add default.nix for vapoursynth-akarin package build.

---
+++ plugins/akarin/default.nix
@@ -0,0 +1,83 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  darwinMinVersionHook,
+  buildPythonPackage,
+  hatchling,
+  meson,
+  ninja,
+  packaging,
+  pkg-config,
+  vapoursynth,
+  libllvm,
+  libxml2,
+  boost,
+  vapoursynth-lib ? builtins.head vapoursynth.buildInputs,
+  withBoostCharconv ? stdenv.hostPlatform.isDarwin,
+}:
+buildPythonPackage {
+  pname = "vapoursynth-akarin";
+  version = "1.4.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "Jaded-Encoding-Thaumaturgy";
+    repo = "akarin-vapoursynth-plugin";
+    rev = "v1.4.1";
+    hash = "sha256-oZi7LerJiLC12qlS3hrwemLtTij23F77a9dVU4MBur4=";
+  };
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail "meson==1.11.0" "meson" \
+      --replace-fail "ninja==1.13.0" "ninja" \
+      --replace-fail "vapoursynth>=74" "vapoursynth"
+    substituteInPlace meson.build \
+      --replace-fail \
+        "py = import('python').find_installation(pure: false)
+r = run_command(
+  py,
+  '-c',
+  'import vapoursynth as vs; print(vs.get_include())',
+  check: true,
+)
+inc_vs = include_directories(r.stdout().strip())
+incdir += inc_vs" \
+        "deps += dependency('vapoursynth')"
+  '';
+
+  nativeBuildInputs = [
+    libllvm.dev
+    libxml2.dev
+    pkg-config
+  ];
+
+  build-system = [
+    hatchling
+    meson
+    ninja
+    packaging
+    vapoursynth
+  ];
+
+  env.MESON_ARGS = lib.optionalString withBoostCharconv "-Dboost-charconv=true";
+
+  buildInputs =
+    [
+      libllvm
+      libxml2.out
+      vapoursynth-lib
+    ]
+    ++ lib.optional withBoostCharconv boost
+    ++ lib.optional (stdenv.hostPlatform.isDarwin && !withBoostCharconv) (darwinMinVersionHook "26.0");
+
+  dependencies = [ vapoursynth ];
+
+  meta = {
+    description = "Enhanced LLVM-based std.Expr and other filters for VapourSynth";
+    homepage = "https://github.com/Jaded-Encoding-Thaumaturgy/akarin-vapoursynth-plugin";
+    license = lib.licenses.lgpl3;
+    platforms = lib.platforms.all;
+  };
+}
