d4555748291016407780716ae501ab4d8f3ba56b
humanlyhuman <humanlyhuman420@pm.me>
2026-04-27 19:34:44 +0200

    Update default.nix

--- plugins/resize2/default.nix
+++ plugins/resize2/default.nix
@@ -1,122 +1,68 @@
 {
   lib,
   stdenv,
-  fetchFromGitHub,
-  buildPythonPackage,
-  meson-python,
-  meson,
-  ninja,
-  packaging,
-  pkg-config,
-  vapoursynth,
-  git,
-  autoreconfHook,
   fetchgit,
-}: let
-  zimg_patched = stdenv.mkDerivation rec {
-    pname = "zimg_patched";
-    version = "unstable-2026-04-27";
+  autoreconfHook,
+  automake,
+  autoconf,
+  libtool,
+  pkg-config,
+}:
 
-    src = fetchgit {
-      url = "https://github.com/sekrit-twc/zimg.git";
-      rev = "df9c1472b9541d0e79c8d02dae37fdf12f189ec2";
-      hash = "sha256-MRWQ6tM1LEL1C4le7Ha7CmiA/V9hXrwp27KgJiHxSes";
-      fetchSubmodules = true;
-    };
+stdenv.mkDerivation rec {
+  pname = "zimg_patched";
+  version = "unstable-2026-04-27";
 
-    nativeBuildInputs = [
-      meson
-      ninja
-      pkg-config
-    ];
+  outputs = [ "out" "dev" ];
 
-    configurePhase = ''
-      meson setup build \
-        --prefix=$out \
-        -Ddefault_library=static
-    '';
-
-    buildPhase = ''
-      ninja -C build
-    '';
-
-    installPhase = ''
-      ninja -C build install
-    '';
-
-    meta = with lib; {
-      description = "Patched zimg fork required by vapoursynth-resize2";
-      homepage = "https://github.com/sekrit-twc/zimg";
-      license = licenses.wtfpl;
-      platforms = platforms.unix;
-    };
+  src = fetchgit {
+    url = "https://github.com/sekrit-twc/zimg.git";
+    rev = "df9c1472b9541d0e79c8d02dae37fdf12f189ec2";
+    hash = "sha256-8PDjDlG3Bso3IQUwjrGqZZR0VtCiVLHB77Ul6n4I+XM=";
+    fetchSubmodules = true;
   };
-in
-  buildPythonPackage rec {
-    pname = "vapoursynth-resize2";
-    version = "0.4.2";
 
-    pyproject = true;
+  nativeBuildInputs = [
+    autoreconfHook
+    automake
+    autoconf
+    libtool
+    pkg-config
+  ];
 
-    src = fetchFromGitHub {
-      owner = "Jaded-Encoding-Thaumaturgy";
-      repo = pname;
-      rev = version;
-      hash = "sha256-oOfDYHBZZ3JEYrbeiwSDNAaua7hlC61lYJOTqB6I7/Q=";
-    };
+  configureFlags = [
+    "--enable-static"
+    "--disable-shared"
+  ];
 
-    nativeBuildInputs = [
-      pkg-config
-      vapoursynth
-      zimg_patched
-    ];
+  postPatch = ''
+    #
+    if [ -f ${./0001.patch} ]; then
+      patch -p1 < ${./0001.patch}
+    fi
+  '';
 
-    build-system = [
-      git
-      meson-python
-      meson
-      ninja
-      packaging
-    ];
+  postInstall = ''
+    mkdir -p $dev/lib/pkgconfig
 
-    buildInputs = [
-      vapoursynth
-      zimg_patched
-    ];
+    cat > $dev/lib/pkgconfig/zimg_patched.pc <<EOF
+    prefix=$out
+    exec_prefix=$out
+    libdir=$out/lib
+    includedir=$dev/include
 
-    propagatedBuildInputs = [
-      vapoursynth
-    ];
+    Name: zimg_patched
+    Description: patched zimg for vapoursynth-resize2
+    Version=${version}
+    Libs: -L$out/lib -lzimg
+    Cflags: -I$dev/include
+    EOF
+  '';
 
-    dontCheckRuntimeDeps = true;
-
-    postPatch = ''
-          python3 <<'EOF'
-      import re
-      p = open("pyproject.toml").read()
-      p = re.sub(r'"vapoursynth>=.*?",?', "", p)
-      p = re.sub(r'"ninja==.*?",?', '"ninja",', p)
-      open("pyproject.toml", "w").write(p)
-      EOF
-
-          substituteInPlace meson.build \
-            --replace-fail \
-            "import vapoursynth as vs; print(vs.get_include())" \
-            "print(\"${vapoursynth}/include/vapoursynth\")"
-    '';
-
-    postInstall = ''
-      mkdir -p $out/lib/vapoursynth
-
-      ln -s \
-        $out/lib/python*/site-packages/vapoursynth/plugins/resize2/libresize2${stdenv.hostPlatform.extensions.sharedLibrary} \
-        $out/lib/vapoursynth/libresize2${stdenv.hostPlatform.extensions.sharedLibrary}
-    '';
-
-    meta = with lib; {
-      description = "resize2 plugin for VapourSynth with blur support";
-      homepage = "https://github.com/Jaded-Encoding-Thaumaturgy/vapoursynth-resize2";
-      license = licenses.lgpl21Only;
-      platforms = platforms.all;
-    };
-  }
+  meta = with lib; {
+    description = "Patched zimg fork used by vapoursynth-resize2";
+    homepage = "https://github.com/sekrit-twc/zimg";
+    license = licenses.wtfpl;
+    platforms = platforms.unix;
+  };
+}
