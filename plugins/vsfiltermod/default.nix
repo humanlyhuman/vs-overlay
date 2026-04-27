66e190dea055db0a1e5590998f378e1604d07b3f
humanlyhuman <humanlyhuman420@pm.me>
2026-04-27 08:39:51 +0200

    Create default.nix for VSFilterMod package
    Add Nix expression for VSFilterMod with dependencies and build configuration.

---
+++ plugins/vsfiltermod/default.nix
@@ -0,0 +1,62 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  pkg-config,
+  vapoursynth,
+  freetype,
+  harfbuzz,
+  fontconfig,
+}:
+stdenv.mkDerivation rec {
+  pname = "vsfiltermod";
+  version = "unstable-2026-03-24";
+
+  src = fetchFromGitHub {
+    owner = "Cinea4678";
+    repo = "VSFilterModButCMake";
+    rev = "93f5a12bde15d4472c0fd8c86c18f6c1de0cf098";
+    sha256 = "sha256-c3NDDG+AgY8ojAZ2fOKWLbByl4/FRIBJTy+zBbsE6dQ=";
+  };
+
+  sse2neon = fetchFromGitHub {
+    owner = "DLTcollab";
+    repo = "sse2neon";
+    rev = "v1.7.0";
+    sha256 = lib.fakeHash;
+  };
+
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+  ];
+
+  buildInputs = [
+    vapoursynth
+    freetype
+    harfbuzz
+    fontconfig
+  ];
+
+  cmakeFlags = [
+    "-DVSFILTERMOD_BUILD_DIRECTSHOW=OFF"
+    "-DCMAKE_CXX_FLAGS=-I${vapoursynth}/include/vapoursynth"
+    "-DCMAKE_SKIP_RPATH=ON"
+    "-Dsse2neon_SOURCE_DIR=${sse2neon}"
+    "-DFETCHCONTENT_FULLY_DISCONNECTED=ON"
+  ];
+
+  postInstall = ''
+    mkdir -p $out/lib/vapoursynth
+    ln -s $out/lib/libvsfm.so $out/lib/vapoursynth/
+  '';
+
+  meta = with lib; {
+    description = "VSFilterMod subtitle renderer with VapourSynth interface and CMake build system";
+    homepage = "https://github.com/Cinea4678/VSFilterModButCMake";
+    license = licenses.gpl3;
+    platforms = platforms.linux;
+    maintainers = [ ];
+  };
+}
