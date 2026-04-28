eaaf413fd55da359efa8946038abfeaf1b70e97c
humanlyhuman <humanlyhuman420@pm.me>
2026-04-28 09:16:06 +0200

    Refactor vardefunc default.nix for clarity

--- plugins/vardefunc/default.nix
+++ plugins/vardefunc/default.nix
@@ -3,58 +3,52 @@
   vapoursynthPlugins,
   buildPythonPackage,
   fetchFromGitHub,
-  vapoursynth,
   setuptools,
-}: let
+  wheel,
+}:
+
+let
   propagatedBinaryPlugins = with vapoursynthPlugins; [
     neo-f3kdb
     ffms2
     scxvid
   ];
 in
-  buildPythonPackage rec {
-    pname = "vardefunc";
-    version = "0.13.0";
-    pyproject = true;
+buildPythonPackage rec {
+  pname = "vardefunc";
+  version = "0.13.0";
+  pyproject = true;
 
-    build-system = [
-      setuptools
-    ];
+  src = fetchFromGitHub {
+    owner = "Ichunjo";
+    repo = pname;
+    rev = version;
+    sha256 = "sha256-Tl2ElO6F3JIIvRLimpRlMXC4/Aju0wNbbXhYxzykWVU=";
+  };
 
-    src = fetchFromGitHub {
-      owner = "Ichunjo";
-      repo = pname;
-      rev = "${version}";
-      sha256 = "sha256-Tl2ElO6F3JIIvRLimpRlMXC4/Aju0wNbbXhYxzykWVU=";
-    };
+  build-system = [
+    setuptools
+    wheel
+  ];
 
-    propagatedBuildInputs =
-      (with vapoursynthPlugins; [
-        lvsfunc
-        vsutil
-      ])
-      ++ propagatedBinaryPlugins;
+  propagatedBuildInputs =
+    (with vapoursynthPlugins; [
+      lvsfunc
+      vsutil
+    ]) ++ propagatedBinaryPlugins;
 
-    postPatch = ''
-      substituteInPlace meson.build \
-        --replace-fail \
-          "run_command(py, '-c', 'import vapoursynth as vs; print(vs.get_include())', check: true).stdout().strip()" \
-          "dependency('vapoursynth').get_variable(pkgconfig: 'includedir')" \
-        --replace-fail \
-          "py.get_install_dir() / 'vapoursynth/plugins'" \
-          "get_option('libdir') / 'vapoursynth'"
-      substituteInPlace requirements.txt \
-          --replace-fail "VapourSynth>=69" ""
-    '';
+  postPatch = ''
+    substituteInPlace requirements.txt \
+      --replace-fail "VapourSynth>=69" ""
+  '';
 
-    checkInputs = [(vapoursynth.withPlugins propagatedBinaryPlugins)];
-    pythonImportsCheck = ["vardefunc"];
+  pythonImportsCheck = [ "vardefunc" ];
 
-    meta = with lib; {
-      description = "Some functions that may be useful";
-      homepage = "https://github.com/Ichunjo/vardefunc";
-      license = licenses.unfree; # no license
-      maintainers = with maintainers; [sbruder];
-      platforms = platforms.all;
-    };
-  }
+  meta = with lib; {
+    description = "Some functions that may be useful";
+    homepage = "https://github.com/Ichunjo/vardefunc";
+    license = licenses.unfree;
+    maintainers = with maintainers; [ sbruder ];
+    platforms = platforms.all;
+  };
+}
