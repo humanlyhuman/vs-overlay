2e61c927eb2628ef396f338c557bb08ceae66c0c
humanlyhuman <humanlyhuman420@pm.me>
2026-04-28 06:22:26 +0200

    Update default.nix

--- plugins/vs-zip/default.nix
+++ plugins/vs-zip/default.nix
@@ -2,14 +2,15 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchzip,
   zig_0_15,
   vapoursynth,
 }: let
-  vsynth-tarball = builtins.fetchurl {
+  vsynth-src = fetchzip {
     url = "https://github.com/dnjulek/vapoursynth-zig/archive/8e93fe3433bb977135f81040bb59d964c58a1cb9.tar.gz";
     sha256 = "sha256-Vnh7Kf00v35Egzmu8dBKVYSHmKmMuVIcmLbtLOOf4bc=";
   };
-  zigimg-tarball = builtins.fetchurl {
+  zigimg-src = fetchzip {
     url = "https://github.com/zigimg/zigimg/archive/362cdd6bce109f7bc674be134cddd378f52da5d4.tar.gz";
     sha256 = "sha256-Er1qOyhm+rpIubkcwWBCoWljFyjVDj+Arp583ocJlLs=";
   };
@@ -28,19 +29,14 @@ in
     zigBuildFlags = ["-Doptimize=ReleaseFast"];
     preBuild = ''
       mkdir -p "$ZIG_GLOBAL_CACHE_DIR/p"
-      cp ${vsynth-tarball} "$ZIG_GLOBAL_CACHE_DIR/p/vapoursynth-4.0.0-jLYMQ799AgCA8sL5lgewK9acIrAKjs-ByT2pdKI5dHq2.tar.gz"
-      cp ${zigimg-tarball} "$ZIG_GLOBAL_CACHE_DIR/p/zigimg-0.1.0-8_eo2jNrFQD4mu3EAUkfQRmCkyfprdIXc8JQ6uyxhjSQ.tar.gz"
+      cp -r ${vsynth-src} "$ZIG_GLOBAL_CACHE_DIR/p/vapoursynth-4.0.0-jLYMQ799AgCA8sL5lgewK9acIrAKjs-ByT2pdKI5dHq2"
+      cp -r ${zigimg-src} "$ZIG_GLOBAL_CACHE_DIR/p/zigimg-0.1.0-8_eo2jNrFQD4mu3EAUkfQRmCkyfprdIXc8JQ6uyxhjSQ"
     '';
     postInstall = ''
       mkdir -p $out/lib/vapoursynth
       mv $out/lib/libvszip${stdenv.hostPlatform.extensions.sharedLibrary} \
          $out/lib/vapoursynth/
     '';
-    postPatch = ''
-      substituteInPlace build.zig \
-        --replace-fail 'lib.linkLibC();' 'lib.root_module.link_libc = true;'
-    '';
-
     meta = {
       description = "VapourSynth zip plugin";
       homepage = "https://github.com/dnjulek/vapoursynth-zip";
