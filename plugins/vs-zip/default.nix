92523009c1168d53b89088adeb588d850b343f19
humanlyhuman <humanlyhuman420@pm.me>
2026-04-28 06:09:14 +0200

    Update default.nix

--- plugins/vs-zip/default.nix
+++ plugins/vs-zip/default.nix
@@ -8,35 +8,16 @@
   zig,
   vapoursynth,
 }: let
-  mkZigDep = name: src:
-    runCommand name {} ''
-      mkdir -p $out
-      cd ${src}
-      tar -czf "$out/${name}.tar.gz" .
-      cp -r . "$out/${name}"
-    '';
-
-  deps = linkFarm "vszip-zig-deps" [
-    (let
-      name = "vapoursynth-4.0.0-jLYMQ799AgCA8sL5lgewK9acIrAKjs-ByT2pdKI5dHq2";
-    in {
-      inherit name;
-      path = mkZigDep name (fetchgit {
-        url = "https://github.com/dnjulek/vapoursynth-zig";
-        rev = "8e93fe3433bb977135f81040bb59d964c58a1cb9";
-        hash = "sha256-tcBr4q7/8u/8xmBO2dbtExi3n5j006nh6/fniO37UK4=";
-      });
-    })
-    (let
-      name = "zigimg-0.1.0-8_eo2jNrFQD4mu3EAUkfQRmCkyfprdIXc8JQ6uyxhjSQ";
-    in {
-      inherit name;
-      path = mkZigDep name (fetchgit {
-        url = "https://github.com/zigimg/zigimg";
-        rev = "362cdd6bce109f7bc674be134cddd378f52da5d4";
-        hash = "sha256-Pe7OAtocaH18FhpSwz3NNZLrBI9BS1rfOehq2UVmu6g=";
-      });
-    })
+vsynth-tarball = mkZigTarball {
+    name = "vapoursynth-4.0.0-jLYMQ799AgCA8sL5lgewK9acIrAKjs-ByT2pdKI5dHq2";
+    url = "git+https://github.com/dnjulek/vapoursynth-zig.git#8e93fe3433bb977135f81040bb59d964c58a1cb9";
+    hash = "sha256:32436a3d3b0fbb46e2dd5eb7cb39145fb6ba856bf342b412c13a76838d3b7704";
+  };
+  zigimg-tarball = mkZigTarball {
+    name = "zigimg-0.1.0-8_eo2jNrFQD4mu3EAUkfQRmCkyfprdIXc8JQ6uyxhjSQ";
+    url = "git+https://github.com/zigimg/zigimg.git#362cdd6bce109f7bc674be134cddd378f52da5d4";
+    hash = "sha256:588d89df8995b7c681064bf0b965cc8fcd24853b7a512eae8fa09aa1f0474a4b";
+  };
   ];
 in
   stdenv.mkDerivation (finalAttrs: {
@@ -55,14 +36,15 @@ in
 
     zigBuildFlags = ["-Doptimize=ReleaseFast"];
 
-    preBuild = ''
-      mkdir -p "$ZIG_GLOBAL_CACHE_DIR/p"
-      for dep in ${deps}/*; do
-        name=$(basename "$dep")
-        ln -s "$dep" "$ZIG_GLOBAL_CACHE_DIR/p/$name"
-        tar -czf "$ZIG_GLOBAL_CACHE_DIR/p/$name.tar.gz" -C "$dep" .
-      done
-    '';
+preBuild = ''
+  mkdir -p "$ZIG_GLOBAL_CACHE_DIR/p"
+
+  cp ${vsynth-tarball} "$ZIG_GLOBAL_CACHE_DIR/p/vapoursynth-4.0.0-jLYMQ799AgCA8sL5lgewK9acIrAKjs-ByT2pdKI5dHq2.tar.gz"
+  tar -xzf ${vsynth-tarball} -C "$ZIG_GLOBAL_CACHE_DIR/p/vapoursynth-4.0.0-jLYMQ799AgCA8sL5lgewK9acIrAKjs-ByT2pdKI5dHq2"
+
+  cp ${zigimg-tarball} "$ZIG_GLOBAL_CACHE_DIR/p/zigimg-0.1.0-8_eo2jNrFQD4mu3EAUkfQRmCkyfprdIXc8JQ6uyxhjSQ.tar.gz"
+  tar -xzf ${zigimg-tarball} -C "$ZIG_GLOBAL_CACHE_DIR/p/zigimg-0.1.0-8_eo2jNrFQD4mu3EAUkfQRmCkyfprdIXc8JQ6uyxhjSQ"
+'';
 
     postInstall = ''
       mkdir -p $out/lib/vapoursynth
