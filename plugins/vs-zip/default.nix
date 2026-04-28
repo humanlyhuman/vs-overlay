bce59d4886a6e2ebf91622e69ecd800b95ed8f76
humanlyhuman <humanlyhuman420@pm.me>
2026-04-28 06:03:14 +0200

    Switch from fetchFromGitHub to fetchgit for dependencies

--- plugins/vs-zip/default.nix
+++ plugins/vs-zip/default.nix
@@ -2,62 +2,62 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchgit,
   linkFarm,
   zig,
   vapoursynth,
-}: let
-  zigDeps = linkFarm "vszip-zig-deps" [
+}:
+let
+  deps = linkFarm "vszip-zig-deps" [
     {
       name = "vapoursynth-4.0.0-jLYMQ799AgCA8sL5lgewK9acIrAKjs-ByT2pdKI5dHq2";
-      path = fetchFromGitHub {
-        owner = "dnjulek";
-        repo = "vapoursynth-zig";
+      path = fetchgit {
+        url = "https://github.com/dnjulek/vapoursynth-zig";
         rev = "8e93fe3433bb977135f81040bb59d964c58a1cb9";
         hash = "sha256-tcBr4q7/8u/8xmBO2dbtExi3n5j006nh6/fniO37UK4=";
       };
     }
     {
       name = "zigimg-0.1.0-8_eo2jNrFQD4mu3EAUkfQRmCkyfprdIXc8JQ6uyxhjSQ";
-      path = fetchFromGitHub {
-        owner = "zigimg";
-        repo = "zigimg";
+      path = fetchgit {
+        url = "https://github.com/zigimg/zigimg";
         rev = "362cdd6bce109f7bc674be134cddd378f52da5d4";
         hash = "sha256-Pe7OAtocaH18FhpSwz3NNZLrBI9BS1rfOehq2UVmu6g=";
       };
     }
   ];
 in
-  stdenv.mkDerivation (finalAttrs: {
-    pname = "vapoursynth-plugin-vszip";
-    version = "R13";
+stdenv.mkDerivation (finalAttrs: {
+  pname = "vapoursynth-plugin-vszip";
+  version = "R13";
 
-    src = fetchFromGitHub {
-      owner = "dnjulek";
-      repo = "vapoursynth-zip";
-      rev = finalAttrs.version;
-      hash = "sha256-k+HfMTn9FLUOCBHFAsSiqHHFF9Q4hUqJpfainN/e2Gc=";
-    };
+  src = fetchFromGitHub {
+    owner = "dnjulek";
+    repo = "vapoursynth-zip";
+    rev = finalAttrs.version;
+    hash = "sha256-k+HfMTn9FLUOCBHFAsSiqHHFF9Q4hUqJpfainN/e2Gc=";
+  };
 
-    nativeBuildInputs = [zig.hook];
-    buildInputs = [vapoursynth];
+  nativeBuildInputs = [ zig.hook ];
+  buildInputs = [ vapoursynth ];
 
-    zigBuildFlags = ["-Doptimize=ReleaseFast"];
+  zigBuildFlags = [ "-Doptimize=ReleaseFast" ];
 
-    postConfigure = ''
-      mkdir -p "$ZIG_GLOBAL_CACHE_DIR/p"
-      ln -s ${zigDeps}/* "$ZIG_GLOBAL_CACHE_DIR/p/"
-    '';
+  preBuild = ''
+    mkdir -p "$ZIG_GLOBAL_CACHE_DIR/p"
+    ln -s ${deps}/* "$ZIG_GLOBAL_CACHE_DIR/p/"
+  '';
 
-    postInstall = ''
-      mkdir -p $out/lib/vapoursynth
-      mv $out/lib/libvszip${stdenv.hostPlatform.extensions.sharedLibrary} \
-         $out/lib/vapoursynth/
-    '';
+  postInstall = ''
+    mkdir -p $out/lib/vapoursynth
+    mv $out/lib/libvszip${stdenv.hostPlatform.extensions.sharedLibrary} \
+       $out/lib/vapoursynth/
+  '';
 
-    meta = {
-      description = "VapourSynth zip/vszip plugin";
-      homepage = "https://github.com/dnjulek/vapoursynth-zip";
-      license = lib.licenses.mit;
-      platforms = lib.platforms.linux;
-    };
-  })
+  meta = {
+    description = "VapourSynth zip plugin";
+    homepage = "https://github.com/dnjulek/vapoursynth-zip";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.linux;
+  };
+})
