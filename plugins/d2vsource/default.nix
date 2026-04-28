08409fcb7fc2ba324c44d113f7b4aa669cb9291d
humanlyhuman <humanlyhuman420@pm.me>
2026-04-28 10:01:14 +0200

    Update default.nix

--- plugins/d2vsource/default.nix
+++ plugins/d2vsource/default.nix
@@ -9,36 +9,52 @@
   vapoursynth,
   python3,
 }:
+
 stdenv.mkDerivation (finalAttrs: {
   pname = "d2vsource";
   version = "1.4";
+
   src = fetchFromGitHub {
     owner = "dwbuiten";
     repo = finalAttrs.pname;
     rev = "v${finalAttrs.version}";
     hash = "sha256-GVMhksXz3Dep9YqgbouEy7d7AuFiHezbkxwjWj1fqvk=";
   };
-  postPatch = ''
-    substituteInPlace meson.build \
-      --replace-fail \
-      "'import vapoursynth as vs; print(vs.get_include())'" \
-      "'print(\"${vapoursynth}/include/vapoursynth\")'"
-  '';
+
   nativeBuildInputs = [
     meson
     ninja
     pkg-config
     python3
   ];
+
   buildInputs = [
     ffmpeg
     vapoursynth
   ];
+
+  postPatch = ''
+    substituteInPlace meson.build \
+      --replace-fail \
+      "'import vapoursynth as vs; print(vs.get_include())'" \
+      "'print(\"${vapoursynth}/include/vapoursynth\")'"
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm755 \
+      libd2vsource${stdenv.hostPlatform.extensions.sharedLibrary} \
+      $out/lib/vapoursynth/libd2vsource${stdenv.hostPlatform.extensions.sharedLibrary}
+
+    runHook postInstall
+  '';
+
   meta = with lib; {
     description = "D2V parser and decoder for VapourSynth";
     homepage = "https://github.com/dwbuiten/d2vsource";
     license = licenses.lgpl21Plus;
-    maintainers = with maintainers; [sbruder];
+    maintainers = with maintainers; [ humanlyhuman ];
     platforms = platforms.all;
   };
 })
