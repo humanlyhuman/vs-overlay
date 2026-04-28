8560ce9042d711ad8b5a0175a44e766a1f622bb3
humanlyhuman <humanlyhuman420@pm.me>
2026-04-28 08:55:41 +0200

    Update build inputs and remove autoreconfHook

--- plugins/d2vsource/default.nix
+++ plugins/d2vsource/default.nix
@@ -2,33 +2,32 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  autoreconfHook,
+  meson,
+  ninja,
   pkg-config,
   ffmpeg,
   vapoursynth,
+  python3,
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "d2vsource";
   version = "1.4";
-
   src = fetchFromGitHub {
     owner = "dwbuiten";
     repo = finalAttrs.pname;
     rev = "v${finalAttrs.version}";
     hash = "sha256-GVMhksXz3Dep9YqgbouEy7d7AuFiHezbkxwjWj1fqvk=";
   };
-
-  configureFlags = ["--libdir=$(out)/lib/vapoursynth"];
-
   nativeBuildInputs = [
-    autoreconfHook
+    meson
+    ninja
     pkg-config
+    python3
   ];
   buildInputs = [
     ffmpeg
     vapoursynth
   ];
-
   meta = with lib; {
     description = "D2V parser and decoder for VapourSynth";
     homepage = "https://github.com/dwbuiten/d2vsource";
