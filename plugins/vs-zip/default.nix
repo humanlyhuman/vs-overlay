72443acc49208bf19c2509ba4091081ca6b11f64
humanlyhuman <humanlyhuman420@pm.me>
2026-04-28 05:37:17 +0200

    Create default.nix for vapoursynth-plugin-vszip
    Add VapourSynth zip/vszip plugin derivation

---
+++ plugins/vs-zip/default.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  zig,
+  vapoursynth,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "vapoursynth-plugin-vszip";
+  version = "R13";
+
+  src = fetchFromGitHub {
+    owner = "dnjulek";
+    repo = "vapoursynth-zip";
+    rev = "R13";
+    hash = "sha256-o3uvZl7BbuePRrkUPNFuxmKqxsFbUzc7lef7cURJ3SY=";
+  };
+
+  nativeBuildInputs = [ zig.hook ];
+  buildInputs = [ vapoursynth ];
+
+  zigBuildFlags = [ "-Doptimize=ReleaseFast" ];
+
+  postInstall = ''
+    mkdir -p $out/lib/vapoursynth
+    mv $out/lib/libvszip${stdenv.hostPlatform.extensions.sharedLibrary} \
+       $out/lib/vapoursynth/libvszip${stdenv.hostPlatform.extensions.sharedLibrary}
+  '';
+
+  meta = {
+    description = "VapourSynth zip/vszip plugin";
+    homepage = "https://github.com/dnjulek/vapoursynth-zip";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.linux;
+    maintainers = [ ];
+  };
+})
