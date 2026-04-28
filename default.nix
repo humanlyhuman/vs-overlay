70e75f3da17aa7a2f673ad7f13cd9aee1a9da0da
humanlyhuman <humanlyhuman420@pm.me>
2026-04-28 05:38:23 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -41,7 +41,7 @@ in {
     vs-ncnn = prev.callPackage ./plugins/vs-mlrt/vsncnn {};
     vs-noise = prev.callPackage ./plugins/vs-noise {};
     vs-trt = prev.callPackage ./plugins/vs-mlrt/vstrt {};
-    vs-zip = prev.callPackage ./plugins/vs-mlrt/vs-zip {};
+    vs-zip = prev.callPackage ./plugins//vs-zip {};
     znedi3 = prev.callPackage ./plugins/znedi3 {};
 
     # Python wrappers
