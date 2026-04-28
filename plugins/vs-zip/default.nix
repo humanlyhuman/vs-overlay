dbd2d7b13b19c4b260fec6299c9f5605cdd81abf
humanlyhuman <humanlyhuman420@pm.me>
2026-04-28 06:05:40 +0200

    Enhance dependency management in default.nix
    Refactor dependency fetching and improve preBuild script.

--- plugins/vs-zip/default.nix
+++ plugins/vs-zip/default.nix
@@ -4,26 +4,35 @@
   fetchFromGitHub,
   fetchgit,
   linkFarm,
+  runCommand,
   zig,
   vapoursynth,
-}: let
+}:
+let
+  mkZigDep = name: src: runCommand name {} ''
+    mkdir -p $out
+    cd ${src}
+    tar -czf "$out/${name}.tar.gz" .
+    cp -r . "$out/${name}"
+  '';
+
   deps = linkFarm "vszip-zig-deps" [
-    {
-      name = "vapoursynth-4.0.0-jLYMQ799AgCA8sL5lgewK9acIrAKjs-ByT2pdKI5dHq2";
-      path = fetchgit {
+    (let name = "vapoursynth-4.0.0-jLYMQ799AgCA8sL5lgewK9acIrAKjs-ByT2pdKI5dHq2"; in {
+      inherit name;
+      path = mkZigDep name (fetchgit {
         url = "https://github.com/dnjulek/vapoursynth-zig";
         rev = "8e93fe3433bb977135f81040bb59d964c58a1cb9";
         hash = "sha256-tcBr4q7/8u/8xmBO2dbtExi3n5j006nh6/fniO37UK4=";
-      };
-    }
-    {
-      name = "zigimg-0.1.0-8_eo2jNrFQD4mu3EAUkfQRmCkyfprdIXc8JQ6uyxhjSQ";
-      path = fetchgit {
+      });
+    })
+    (let name = "zigimg-0.1.0-8_eo2jNrFQD4mu3EAUkfQRmCkyfprdIXc8JQ6uyxhjSQ"; in {
+      inherit name;
+      path = mkZigDep name (fetchgit {
         url = "https://github.com/zigimg/zigimg";
         rev = "362cdd6bce109f7bc674be134cddd378f52da5d4";
         hash = "sha256-Pe7OAtocaH18FhpSwz3NNZLrBI9BS1rfOehq2UVmu6g=";
-      };
-    }
+      });
+    })
   ];
 in
   stdenv.mkDerivation (finalAttrs: {
@@ -41,10 +50,14 @@ in
     buildInputs = [vapoursynth];
 
     zigBuildFlags = ["-Doptimize=ReleaseFast"];
-
+    
     preBuild = ''
       mkdir -p "$ZIG_GLOBAL_CACHE_DIR/p"
-      ln -s ${deps}/* "$ZIG_GLOBAL_CACHE_DIR/p/"
+      for dep in ${deps}/*; do
+        name=$(basename $dep)
+        ln -s "$dep" "$ZIG_GLOBAL_CACHE_DIR/p/$name"
+        tar -czf "$ZIG_GLOBAL_CACHE_DIR/p/${name}.tar.gz" -C "$dep" .
+      done
     '';
 
     postInstall = ''
@@ -52,6 +65,7 @@ in
       mv $out/lib/libvszip${stdenv.hostPlatform.extensions.sharedLibrary} \
          $out/lib/vapoursynth/
     '';
+
 
     meta = {
       description = "VapourSynth zip plugin";
