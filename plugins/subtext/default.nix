c282b093ddafa7cfc61d17bf8eb424e7744a3f53
humanlyhuman <humanlyhuman420@pm.me>
2026-04-28 09:59:39 +0200

    Update default.nix

--- plugins/subtext/default.nix
+++ plugins/subtext/default.nix
@@ -10,25 +10,35 @@
   libass,
   vapoursynth,
 }:
+
 stdenv.mkDerivation rec {
   pname = "subtext";
   version = "6";
+
   src = fetchFromGitHub {
     owner = "vapoursynth";
     repo = pname;
     rev = "R${version}";
     hash = "sha256-MX1QQ0h82PLXasYrnFbrkaeAXmGENCxyqWmnVUP27dY=";
   };
+
   nativeBuildInputs = [
     meson
     ninja
     pkg-config
     python3
   ];
+
+  buildInputs = [
+    ffmpeg
+    libass
+    vapoursynth
+  ];
+
   postPatch = ''
-      substituteInPlace meson.build \
-          --replace-fail \
-              "incdir = include_directories(
+    substituteInPlace meson.build \
+      --replace-fail \
+      "incdir = include_directories(
         run_command(
             find_program('python', 'python3'),
             '-c',
@@ -36,18 +46,24 @@ stdenv.mkDerivation rec {
             check: true,
         ).stdout().strip(),
     )" \
-              "incdir = include_directories('${vapoursynth}/include/vapoursynth')"
+      "incdir = include_directories('${vapoursynth}/include/vapoursynth')"
   '';
-  buildInputs = [
-    ffmpeg
-    libass
-    vapoursynth
-  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm755 \
+      libsubtext${stdenv.hostPlatform.extensions.sharedLibrary} \
+      $out/lib/vapoursynth/libsubtext${stdenv.hostPlatform.extensions.sharedLibrary}
+
+    runHook postInstall
+  '';
+
   meta = with lib; {
     description = "Subtitle plugin for VapourSynth based on libass";
     homepage = "https://github.com/vapoursynth/subtext";
     license = licenses.mit;
-    maintainers = with maintainers; [sbruder];
+    maintainers = with maintainers; [ humanlyhuman ];
     platforms = platforms.all;
   };
 }
