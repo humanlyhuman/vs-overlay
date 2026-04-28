f6144559af7ae1d99950074484fc6f7bed94305c
humanlyhuman <humanlyhuman420@pm.me>
2026-04-28 08:44:47 +0200

    Refactor flake.nix for improved readability

--- flake.nix
+++ flake.nix
@@ -1,18 +1,34 @@
 {
   description = "vs-overlay";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://humanlyhuman-vs-repo.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "humanlyhuman-vs-repo.cachix.org-1:3Dep7SJrZH4ao/jMu9KF4vd8Ul0AHlSj6GNMISDP96I="
+    ];
+  };
+
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
   outputs = {
     self,
     nixpkgs,
   } @ inputs: let
     inherit (nixpkgs) lib;
+
     buildPlatforms = import ./lib/platforms.nix inputs;
-    eachSystem = uwu: lib.mapAttrs uwu buildPlatforms;
+
+    eachSystem = f: lib.mapAttrs f buildPlatforms;
+
     pkgs = eachSystem (
       system: _:
         import nixpkgs {
           inherit system;
-          overlays = [self.overlays.default];
+          overlays = [ self.overlays.default ];
+
           config = {
             allowUnfree = true;
             allowUnsupportedSystem = true;
@@ -21,7 +37,10 @@
     );
   in {
     overlays.default = import ./default.nix;
-    formatter = eachSystem (system: _: pkgs.${system}.nixfmt-tree);
+
+    formatter = eachSystem (
+      system: _: pkgs.${system}.nixfmt-tree
+    );
 
     packages = eachSystem (
       system: platform:
@@ -30,10 +49,13 @@
             inherit (pkgs.${system}) getnative;
             inherit (pkgs.${system}) nativeres;
           }
-          // lib.filterAttrs (_: lib.isDerivation) pkgs.${system}.vapoursynthPlugins
+          // lib.filterAttrs (_: lib.isDerivation)
+            pkgs.${system}.vapoursynthPlugins
         )
     );
 
-    hydraJobs = {inherit (self) packages;};
+    hydraJobs = {
+      inherit (self) packages;
+    };
   };
 }
