d076de4e1d391d0ac5a4fa8be64dbbd6cec8a98a
humanlyhuman <humanlyhuman420@pm.me>
2026-04-28 06:14:07 +0200

    Update default.nix

--- plugins/vs-zip/default.nix
+++ plugins/vs-zip/default.nix
@@ -2,21 +2,16 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  fetchgit,
-  linkFarm,
-  runCommand,
   zig,
   vapoursynth,
 }: let
-  vsynth-src = fetchgit {
-    url = "https://github.com/dnjulek/vapoursynth-zig.git";
-    rev = "8e93fe3433bb977135f81040bb59d964c58a1cb9";
-    hash = "sha256-tcBr4q7/8u/8xmBO2dbtExi3n5j006nh6/fniO37UK4=";
+  vsynth-tarball = builtins.fetchurl {
+    url = "https://github.com/dnjulek/vapoursynth-zig/archive/8e93fe3433bb977135f81040bb59d964c58a1cb9.tar.gz";
+    sha256 = "sha256-MkNqPTsPu0bi3V63yzkUX7a6hWvzQrQSwTp2g407dwQ=";
   };
-  zigimg-src = fetchgit {
-    url = "https://github.com/zigimg/zigimg.git";
-    rev = "362cdd6bce109f7bc674be134cddd378f52da5d4";
-    hash = "sha256-Pe7OAtocaH18FhpSwz3NNZLrBI9BS1rfOehq2UVmu6g=";
+  zigimg-tarball = builtins.fetchurl {
+    url = "https://github.com/zigimg/zigimg/archive/362cdd6bce109f7bc674be134cddd378f52da5d4.tar.gz";
+    sha256 = "sha256-WI2J34mVt8aBBkvwuWXMj80khTt6US6uj6CaofBHSks=";
   };
 in
   stdenv.mkDerivation (finalAttrs: {
@@ -33,8 +28,8 @@ in
     zigBuildFlags = ["-Doptimize=ReleaseFast"];
     preBuild = ''
       mkdir -p "$ZIG_GLOBAL_CACHE_DIR/p"
-      cp -r ${vsynth-src} "$ZIG_GLOBAL_CACHE_DIR/p/vapoursynth-4.0.0-jLYMQ799AgCA8sL5lgewK9acIrAKjs-ByT2pdKI5dHq2"
-      cp -r ${zigimg-src} "$ZIG_GLOBAL_CACHE_DIR/p/zigimg-0.1.0-8_eo2jNrFQD4mu3EAUkfQRmCkyfprdIXc8JQ6uyxhjSQ"
+      cp ${vsynth-tarball} "$ZIG_GLOBAL_CACHE_DIR/p/vapoursynth-4.0.0-jLYMQ799AgCA8sL5lgewK9acIrAKjs-ByT2pdKI5dHq2.tar.gz"
+      cp ${zigimg-tarball} "$ZIG_GLOBAL_CACHE_DIR/p/zigimg-0.1.0-8_eo2jNrFQD4mu3EAUkfQRmCkyfprdIXc8JQ6uyxhjSQ.tar.gz"
     '';
     postInstall = ''
       mkdir -p $out/lib/vapoursynth
