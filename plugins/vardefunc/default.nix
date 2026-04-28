ee391f3c913de8702c190676e3e3a86662d55e14
humanlyhuman <humanlyhuman420@pm.me>
2026-04-28 09:16:48 +0200

    Refactor vardefunc default.nix for dependencies and build system

--- plugins/vardefunc/default.nix
+++ plugins/vardefunc/default.nix
@@ -1,53 +1,50 @@
 {
   lib,
-  vapoursynthPlugins,
   buildPythonPackage,
   fetchFromGitHub,
-  setuptools,
-  wheel,
-}: let
-  propagatedBinaryPlugins = with vapoursynthPlugins; [
-    neo-f3kdb
-    ffms2
-    scxvid
+  hatchling,
+  versioningit,
+  numpy,
+  pytimeconv,
+  vapoursynth,
+  vsjetpack,
+  lvsfunc,
+}:
+
+buildPythonPackage rec {
+  pname = "vardefunc";
+  version = "0.13.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "Ichunjo";
+    repo = pname;
+    rev = version;
+    sha256 = "sha256-Tl2ElO6F3JIIvRLimpRlMXC4/Aju0wNbbXhYxzykWVU=";
+  };
+
+  build-system = [
+    hatchling
+    versioningit
   ];
-in
-  buildPythonPackage rec {
-    pname = "vardefunc";
-    version = "0.13.0";
-    pyproject = true;
 
-    src = fetchFromGitHub {
-      owner = "Ichunjo";
-      repo = pname;
-      rev = version;
-      sha256 = "sha256-Tl2ElO6F3JIIvRLimpRlMXC4/Aju0wNbbXhYxzykWVU=";
-    };
+  dependencies = [
+    numpy
+    pytimeconv
+    vapoursynth
+    vsjetpack
+  ];
 
-    build-system = [
-      setuptools
-      wheel
-    ];
+  optional-dependencies = {
+    comp = [ lvsfunc ];
+  };
 
-    propagatedBuildInputs =
-      (with vapoursynthPlugins; [
-        lvsfunc
-        vsutil
-      ])
-      ++ propagatedBinaryPlugins;
+  pythonImportsCheck = [ "vardefunc" ];
 
-    postPatch = ''
-      substituteInPlace pyproject.toml \
-        --replace-fail "VapourSynth>=69" ""
-    '';
-
-    pythonImportsCheck = ["vardefunc"];
-
-    meta = with lib; {
-      description = "Some functions that may be useful";
-      homepage = "https://github.com/Ichunjo/vardefunc";
-      license = licenses.unfree;
-      maintainers = with maintainers; [sbruder];
-      platforms = platforms.all;
-    };
-  }
+  meta = with lib; {
+    description = "Vardë's Vapoursynth functions";
+    homepage = "https://github.com/Ichunjo/vardefunc";
+    license = licenses.mit;
+    platforms = platforms.all;
+  };
+}
