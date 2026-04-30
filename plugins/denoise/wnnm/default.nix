59f207a1881c3af8198dcc51b651dfd20160a17f
Your Name <you@example.com>
2026-04-30 10:52:01 +0200

    fix: format build-system and remove unnecessary buildInputs in wnnm plugin

--- plugins/denoise/wnnm/default.nix
+++ plugins/denoise/wnnm/default.nix
@@ -6,18 +6,24 @@
   hatchling,
   buildPythonPackage,
 }:
+
 buildPythonPackage rec {
   pname = "wnnm";
   version = "3";
+
   pyproject = true;
-  build-system = ["hatchling"];
+
+  build-system = [
+    hatchling
+  ];
+
   src = fetchFromGitHub {
     owner = "AmusementClub";
     repo = "VapourSynth-WNNM";
     rev = "v${version}";
     hash = "sha256-fPtHaDrG1Ku1/Uv0Bh3hUfqbOEyfnhFVFblspRhHqlE=";
   };
-  buildInputs = [ vapoursynth ];
+
   meta = with lib; {
     description = "Weighted Nuclear Norm Minimization Denoiser for VapourSynth.";
     homepage = "https://github.com/AmusementClub/VapourSynth-WNNM";
